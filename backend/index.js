5e88ceb9c90bc2a023b56cbe210e0c374bd4482e
pulkit161001 <pulkit161001@gmail.com>
2026-01-19 00:17:24 +0530

    backend path

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,12 @@ import {
 import axios from "axios";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
 
 const app = express();
 
