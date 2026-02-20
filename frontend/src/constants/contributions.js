166eeb19ad9d629aef93851162457bcbea62467c
Take2 <pulkit@trytaketwo.com>
2026-02-20 15:44:26 +0530

    chore: remove unused contributions constant

--- frontend/src/constants/contributions.js
+++
@@ -1,8 +0,0 @@
-export const contributions = [
-  {
-    username: "pulkit161001",
-    userAvatar: "https://assets.leetcode.com/users/avatars/avatar_1675590784.png",
-    realName: "Pulkit Malhotra",
-    reputation: 224,
-  },
-];
